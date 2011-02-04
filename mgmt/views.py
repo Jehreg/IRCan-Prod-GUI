dcc96d02e23eb6d4a0a1fc841b961e91e542793e
Gabriel Filion <lelutin@gmail.com>
2011-02-04 16:26:03 -0500

    Fix logout
    Currently, the login/logout feature doesn't look like it's working.
    
    Pass the main page's url to the logout view so that users get redirected
    there instead of being stuck on a page that looks like the admin
    interface.
    
    Make logged out users see a page without the left column, since the list
    of machines is irrelevant in this context.
    
    Also, the 'request' object is not automatically passed on to the
    templates, so the user name is always empty. Fix this and get the user's
    name appropriately.

--- mgmt/views.py
+++ mgmt/views.py
@@ -3,4 +3,10 @@ from django.conf import settings
 
 def main(request):
     # wth, MEDIA_URL is supposed to be automatically in the context.
-    return render_to_response('index.html', {"MEDIA_URL": settings.MEDIA_URL})
+    if request.user.is_authenticated():
+        ctx = {"first_name": request.user.first_name,
+               "last_name": request.user.last_name,
+               "MEDIA_URL": settings.MEDIA_URL}
+        return render_to_response('inside.html', ctx)
+    else:
+        return render_to_response('outside.html', {"MEDIA_URL": settings.MEDIA_URL})
--- urls.py
+++ urls.py
@@ -20,7 +20,7 @@ urlpatterns += patterns('',
     (r'^admin/', include(admin.site.urls)),
 
     url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name="logout"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {"next_page": '/'}, name="logout"),
 )
 
 if settings.DEBUG:
